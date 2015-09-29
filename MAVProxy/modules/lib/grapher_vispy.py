e23af886b9ca009c1e5f186c4914081cc1dfbf43
SamuelDudley <dudley.samuel@gmail.com>
2015-09-29 13:12:50 +0930

    trying to get att's working for np arrays

---
+++ MAVProxy/modules/lib/grapher_vispy.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+
+'''
+ core library for graphing in mavexplorer
+'''
+import numpy as np
+import vispy.plot as vp
+from vispy.color import get_colormap
+
+import sys, struct, time, os, datetime
+import math, re
+from math import *
+from pymavlink.mavextra import *
+from pymavlink import mavutil
+
+
+colourmap = {
+    'apm' : {
+        'MANUAL'    : (1.0,   0,   0),
+        'AUTO'      : (  0, 1.0,   0),
+        'LOITER'    : (  0,   0, 1.0),
+        'FBWA'      : (1.0, 0.5,   0),
+        'RTL'       : (  1,   0, 0.5),
+        'STABILIZE' : (0.5, 1.0,   0),
+        'LAND'      : (  0, 1.0, 0.5),
+        'STEERING'  : (0.5,   0, 1.0),
+        'HOLD'      : (  0, 0.5, 1.0),
+        'ALT_HOLD'  : (1.0, 0.5, 0.5),
+        'CIRCLE'    : (0.5, 1.0, 0.5),
+        'POSITION'  : (1.0, 0.0, 1.0),
+        'GUIDED'    : (0.5, 0.5, 1.0),
+        'ACRO'      : (1.0, 1.0,   0),
+        'CRUISE'    : (  0, 1.0, 1.0)
+        },
+    'px4' : {
+        'MANUAL'    : (1.0,   0,   0),
+        'SEATBELT'  : (  0.5, 0.5,   0),
+        'EASY'      : (  0, 1.0,   0),
+        'AUTO'    : (  0,   0, 1.0),
+        'UNKNOWN'    : (  1.0,   1.0, 1.0)
+        }
+    }
+
+class MavGraphVispy(object):
+    def __init__(self):
+        # create figure with plot
+        self.fig = vp.Fig(bgcolor='w', size=(800, 600), show=True)
+        self.plt = self.fig[0, 0]
+        self.plt._configure_2d()
+        self.plt.title.text = ''
+        self.plt.ylabel.text = ''
+        self.plt.xlabel.text = ''
+        self.selected = None
+        self.fig.connect(self.on_mouse_press)
+        self.fig.connect(self.on_mouse_move)
+        self.fig.connect(self.on_mouse_wheel)
+        self.linked = False
+        self.fields = []
+        self.labels = None
+        
+    def add_field(self, field):
+        '''add another field to plot'''
+        self.fields.append(field)
+    
+    def process(self, block=True):
+        '''process and display graph'''
+        self.msg_types = set()
+        self.multiplier = []
+        self.field_types = []
+
+        # work out msg types we are interested in
+        self.x = []
+        self.y = []
+        self.modes = []
+        self.axes = []
+        self.first_only = []
+        re_caps = re.compile('[A-Z_][A-Z0-9_]+')
+        for f in self.fields:
+            caps = set(re.findall(re_caps, f))
+            self.msg_types = self.msg_types.union(caps)
+            self.field_types.append(caps)
+        print self.field_types
+#             self.y.append([])
+#             self.x.append([])
+#             self.axes.append(1)
+#             self.first_only.append(False)
+# 
+#         if self.labels is not None:
+#             labels = self.labels.split(',')
+#             if len(labels) != len(fields)*len(self.mav_list):
+#                 print("Number of labels (%u) must match number of fields (%u)" % (
+#                     len(labels), len(fields)*len(self.mav_list)))
+#                 return
+#         else:
+#             labels = None
+#             
+#         timeshift = self.timeshift
+# 
+#         for fi in range(0, len(self.mav_list)):
+#             mlog = self.mav_list[fi]
+#             self.process_mav(mlog, timeshift)
+#             timeshift = 0
+#             for i in range(0, len(self.x)):
+#                 if self.first_only[i] and fi != 0:
+#                     self.x[i] = []
+#                     self.y[i] = []
+#             if labels:
+#                 lab = labels[fi*len(self.fields):(fi+1)*len(self.fields)]
+#             else:
+#                 lab = self.fields[:]
+#             if self.multi:
+#                 col = colors[:]
+#             else:
+#                 col = colors[fi*len(self.fields):]
+#             self.plotit(self.x, self.y, lab, colors=col)
+#             for i in range(0, len(self.x)):
+#                 self.x[i] = []
+#                 self.y[i] = []
+#         pylab.draw()
+
+    def add_data(self, vars):
+        '''add some data'''
+        for i in range(0, len(self.fields)):
+            f = self.fields[i]
+            v = evaluate_expression(f, vars)
+            print 'v',v
+
+    
+    def set_data(self, t, data, lables=None):
+        self.t=t
+        self.data = data
+        self.lables = lables
+        # plot data
+        cmap = get_colormap('hsl', value=0.5)
+        colors = cmap.map(np.linspace(0.1, 0.9, self.data.shape[0]))
+        
+        for i, y in enumerate(self.data):
+            line = self.plt.plot((self.t, y), color=colors[i])
+            line.interactive = True
+            line.unfreeze()  # make it so we can add a new property to the instance
+            line.data_index = i
+            line.freeze()
+        
+        
+        # Build visuals used for cursor
+        self.cursor_text = vp.Text("", pos=(0, 0), anchor_x='left', anchor_y='center',
+                              font_size=8, parent=self.plt.view.scene)
+        self.cursor_line = vp.Line(parent=self.plt.view.scene)
+        self.cursor_symbol = vp.Markers(pos=np.array([[0, 0]]), parent=self.plt.view.scene)
+        self.cursor_line.visible = False
+        self.cursor_symbol.visible = False
+        self.cursor_line.order = 10
+        self.cursor_symbol.order = 11
+        self.cursor_text.order = 10
+        
+    def on_mouse_press(self, event):
+        if event.handled or event.button != 1:
+            return
+        if self.selected is not None:
+            self.selected.set_data(width=1)
+        self.selected = None
+        for v in self.fig.visuals_at(event.pos):
+            if isinstance(v, vp.LinePlot):
+                self.selected = v
+                break
+        if self.selected is not None:
+            self.selected.set_data(width=3)
+            self.update_cursor(event.pos)
+    
+    def on_mouse_wheel(self, event):
+        self.update_cursor(event.pos)
+    
+    def on_mouse_move(self, event):
+        self.update_cursor(event.pos)
+    
+    
+    def update_cursor(self, pos):
+        if self.selected is None:
+            self.cursor_text.visible = False
+            self.cursor_line.visible = False
+            self.cursor_symbol.visible = False
+        else:
+            # get data for the selected trace
+            trace = self.data[self.selected.data_index]
+            
+            # map the mouse position to data coordinates
+            tr = self.fig.scene.node_transform(self.selected)
+            pos = tr.map(pos)
+            
+            # get interpolated y coordinate
+            x_cur = pos[0]
+            
+          
+            i = find_nearest(self.t,x_cur)
+            
+            x = self.t[i]
+            y = trace[i]
+            
+            # update cursor
+            if self.lables:
+                data_label= self.lables[self.selected.data_index]
+                self.cursor_text.text = "%s: x=%0.2f, y=%0.2f" % (data_label,x, y)
+            else:
+                self.cursor_text.text = "%i: x=%0.2f, y=%0.2f" % (self.selected.data_index,x, y)
+            offset = np.diff(tr.map([[0, 0], [10, 0]]), axis=0)[0, 0]
+            self.cursor_text.pos = x + offset, y
+            rect = self.plt.view.camera.rect
+            self.cursor_line.set_data(np.array([[x, rect.bottom], [x, rect.top]]))
+            self.cursor_symbol.set_data(pos=np.array([[x, y]]), symbol='+',
+                                   face_color='b')
+            self.cursor_text.visible = True
+            self.cursor_line.visible = True
+            self.cursor_symbol.visible = True
+    
+    def show(self):
+        self.fig.app.run(allow_interactive=True)
+
+def find_nearest(array,value):
+    idx = (np.abs(array-value)).argmin()
+    return idx
+
+def evaluate_expression(expression, vars): #nicked from mavutil
+    '''evaluation an expression'''
+    try:
+        v = eval(expression, globals(), vars)
+    except NameError:
+        return None
+    except ZeroDivisionError:
+        return None
+    return v
---
+++ MAVProxy/tools/MAVHawkview.py
@@ -0,0 +1,484 @@
+#!/usr/bin/env python
+'''
+log analysis program
+based on mavexplorer by Andrew Tridgell 
+Samuel Dudley September 2015
+'''
+import numpy as np
+import sys, struct, time, os, datetime
+import math, re
+import Queue
+import fnmatch
+import threading, multiprocessing
+from math import *
+from MAVProxy.modules.lib import rline
+from MAVProxy.modules.lib import wxconsole
+from MAVProxy.modules.lib import grapher
+from MAVProxy.modules.lib import mavmemlog
+from pymavlink.mavextra import *
+from MAVProxy.modules.lib.mp_menu import *
+from pymavlink import mavutil
+from MAVProxy.modules.lib.mp_settings import MPSettings, MPSetting
+from MAVProxy.modules.lib import wxsettings
+from lxml import objectify
+import pkg_resources
+
+import numpy as np
+from MAVProxy.modules.lib import grapher_vispy
+ 
+class MEStatus(object):
+    '''status object to conform with mavproxy structure for modules'''
+    def __init__(self):
+        self.msgs = {}
+
+class MEState(object):
+    '''holds state of MAVExplorer'''
+    def __init__(self):
+        self.message_count = {}
+        self.message_field_count = {}
+        self.arrays = np.zeros(1,)
+        self.input_queue = Queue.Queue()
+        self.rl = None
+        self.console = wxconsole.MessageConsole(title='MAVHawkview')
+        self.exit = False
+        self.status = MEStatus()
+        self.settings = MPSettings(
+            [ MPSetting('marker', str, '+', 'data marker', tab='Graph'),
+              MPSetting('condition', str, None, 'condition'),
+              MPSetting('xaxis', str, None, 'xaxis'),
+              MPSetting('linestyle', str, None, 'linestyle'),
+              MPSetting('flightmode', str, None, 'flightmode', choice=['apm','px4']),
+              MPSetting('legend', str, 'upper left', 'legend position'),
+              MPSetting('legend2', str, 'upper right', 'legend2 position')
+              ]
+            )
+
+        self.mlog = None
+        self.command_map = command_map
+        self.completions = {
+            "set"       : ["(SETTING)"],
+            "condition" : ["(VARIABLE)"],
+            "graph"     : ['(VARIABLE) (VARIABLE) (VARIABLE) (VARIABLE) (VARIABLE) (VARIABLE)'],
+            "map"       : ['(VARIABLE) (VARIABLE) (VARIABLE) (VARIABLE) (VARIABLE)']
+            }
+        self.aliases = {}
+        self.graphs = []
+        self.flightmode_selections = []
+
+def have_graph(name):
+    '''return true if we have a graph of the given name'''
+    for g in mestate.graphs:
+        if g.name == name:
+            return True
+    return False
+
+def menu_callback(m):
+    '''called on menu selection'''
+    if m.returnkey.startswith('# '):
+        cmd = m.returnkey[2:]
+        if m.handler is not None:
+            if m.handler_result is None:
+                return
+            cmd += m.handler_result
+        process_stdin(cmd)
+    elif m.returnkey == 'menuSettings':
+        wxsettings.WXSettings(mestate.settings)
+    elif m.returnkey.startswith("mode-"):
+        idx = int(m.returnkey[5:])
+        mestate.flightmode_selections[idx] = m.IsChecked()
+    else:
+        print('Unknown menu selection: %s' % m.returnkey)
+
+
+def flightmode_menu():
+    '''construct flightmode menu'''
+    modes = mestate.mlog.flightmode_list()
+    ret = []
+    idx = 0
+    for (mode,t1,t2) in modes:
+        modestr = "%s %us" % (mode, (t2-t1))
+        ret.append(MPMenuCheckbox(modestr, modestr, 'mode-%u' % idx))
+        idx += 1
+        mestate.flightmode_selections.append(False)
+    return ret
+
+def messages_menu():
+    '''construct messages menu'''
+    msgs = mestate.message_field_count.keys()
+    ret = []
+    idx = 0
+    for msg in msgs:
+        msgstr = "%s" % (msg)
+        ret.append(MPMenuCheckbox(msgstr,msgstr, 'mode-%u' % idx))
+        idx += 1
+    return ret
+
+class graph_tree_state(object):
+    def __init__(self, graphs):
+        self.prefix = None
+        self.graphs = graphs[:]
+
+def graph_menus():
+    '''return menu tree for graphs (recursive)'''
+    ret = MPMenuSubMenu('Graphs', [])
+    for i in range(len(mestate.graphs)):
+        g = mestate.graphs[i]
+        path = g.name.split('/')
+        name = path[-1]
+        path = path[:-1]
+        ret.add_to_submenu(path, MPMenuItem(name, name, '# graph :%u' % i))
+    return ret
+
+def setup_menus():
+    '''setup console menus'''
+    menu = MPMenuTop([])
+    menu.add(MPMenuSubMenu('MAVExplorer',
+                           items=[MPMenuItem('Settings', 'Settings', 'menuSettings'),
+                                  MPMenuItem('Map', 'Map', '# map')]))
+
+    menu.add(graph_menus())
+    menu.add(MPMenuSubMenu('FlightMode', items=flightmode_menu()))
+    
+    menu.add(MPMenuSubMenu('Messages', items=messages_menu()))
+
+    mestate.console.set_menu(menu, menu_callback)
+
+class GraphDefinition(object):
+    '''a pre-defined graph'''
+    def __init__(self, name, expression, description):
+        self.name = name
+        self.expression = expression
+        self.description = description
+
+def load_graph_xml(xml):
+    '''load a graph from one xml string'''
+    try:
+        root = objectify.fromstring(xml)
+    except Exception:
+        return False
+    if root.tag != 'graphs':
+        return False
+    if not hasattr(root, 'graph'):
+        return False
+    for g in root.graph:
+        name = g.attrib['name']
+        if have_graph(name):
+            continue
+        expressions = [e.text for e in g.expression]
+        for e in expressions:
+            graph_ok = True
+            fields = e.split()
+            for f in fields:
+                try:
+                    if f.endswith(':2'):
+                        f = f[:-2]
+                    if mavutil.evaluate_expression(f, mestate.status.msgs) is None:
+                        graph_ok = False                        
+                except Exception:
+                    graph_ok = False
+                    break
+            if graph_ok:
+                mestate.graphs.append(GraphDefinition(name, e, g.description.text))
+                break
+    return True
+
+def count_msg_types():
+    for msg in mestate.mlog._msgs:
+        type = msg.get_type()
+        if type not in mestate.message_count.keys():
+            mestate.message_count[type] = 0
+#             f='GPS.Spd'
+#             vars = mlog.messages
+#             print vars
+#             v = mavutil.evaluate_expression(f, vars)
+#             #print v
+            #print len(msg.fmt.columns),msg.fmt.columns
+            mestate.message_field_count[type] = msg.fmt.columns
+        mestate.message_count[type] += 1
+    #print mestate.message_count
+    #print mestate.message_field_count #this holds a list of the field names for each msg type
+    
+    t2 = time.time()
+    mestate.arrays = {}
+    array_idx = {}
+    
+    ignore_list = ['FMT','MSG','PARM']
+    msgs_to_parse = [x for x in mestate.message_field_count.keys() if x not in ignore_list]
+    msgs_to_parse_dict = {}
+    
+    for msg in msgs_to_parse:
+        msgs_to_parse_dict[msg] = mestate.message_field_count[msg]
+
+    for msg_type in msgs_to_parse:
+        mestate.arrays[msg_type] = np.zeros((len(msgs_to_parse_dict[msg_type]),mestate.message_count[msg_type]))
+        array_idx[msg_type] = 0
+    
+    for msg in mestate.mlog._msgs:
+        msg_type = msg.get_type()
+        if msg_type in msgs_to_parse:
+            for (idx,c) in enumerate(msgs_to_parse_dict[msg_type]):
+                    try:
+                        mestate.arrays[msg_type][idx][array_idx[msg_type]]=msg.__getattr__(c)
+                    except:
+                        print type
+                
+            array_idx[msg_type] +=1
+
+    t3 = time.time()
+    
+    
+
+    print 'time', t3-t2
+    
+class MEData(object):
+    def __init__(self):
+        self.type = None
+        self.data = np.array([])
+        
+    def get_type(self):
+        return self.type
+    
+    
+def load_graphs():
+    '''load graphs from mavgraphs.xml'''
+    mestate.graphs = []
+    gfiles = ['mavgraphs.xml']
+    if 'HOME' in os.environ:
+        for dirname, dirnames, filenames in os.walk(os.path.join(os.environ['HOME'], ".mavproxy")):
+            for filename in filenames:
+                if filename.lower().endswith('.xml'):
+                    gfiles.append(os.path.join(dirname, filename))
+    for file in gfiles:
+        if not os.path.exists(file):
+            continue
+        if load_graph_xml(open(file).read()):
+            mestate.console.writeln("Loaded %s" % file)
+    # also load the built in graphs
+    dlist = pkg_resources.resource_listdir("MAVProxy", "tools/graphs")
+    for f in dlist:
+        raw = pkg_resources.resource_stream("MAVProxy", "tools/graphs/%s" % f).read()
+        if load_graph_xml(raw):
+            mestate.console.writeln("Loaded %s" % f)
+    mestate.graphs = sorted(mestate.graphs, key=lambda g: g.name)
+
+
+def graph_process_vispy(fields):
+    '''process for a vispy graph'''
+    mgv = grapher_vispy.MavGraphVispy()
+    for f in fields:
+        mgv.add_field(f)
+    
+    mgv.add_data(mestate.arrays)
+    #print fields
+    plt_idx =[]
+    plt_lables =[]
+        
+    for field in fields:
+        field_parts = field.split('.')
+        #print field_parts
+        #print mestate.message_field_count[field_parts[0]]
+        plt_idx.append(mestate.message_field_count[field_parts[0]].index(field_parts[1]))
+        plt_lables.append(field)
+        time_ms = mestate.message_field_count[field_parts[0]].index('TimeMS')
+    
+    y = mestate.arrays[field_parts[0]][plt_idx,:]
+    x = mestate.arrays[field_parts[0]][time_ms,:]
+    mgv.set_data(x,y,lables = plt_lables)
+    
+
+
+    mgv.process()
+    mgv.show()
+
+
+def graph_process(fields):
+    '''process for a graph'''
+    mestate.mlog.reduce_by_flightmodes(mestate.flightmode_selections)
+    
+    mg = grapher.MavGraph()
+    mg.set_marker(mestate.settings.marker)
+    mg.set_condition(mestate.settings.condition)
+    mg.set_xaxis(mestate.settings.xaxis)
+    mg.set_linestyle(mestate.settings.linestyle)
+    mg.set_flightmode(mestate.settings.flightmode)
+    mg.set_legend(mestate.settings.legend)
+    mg.add_mav(mestate.mlog)
+    for f in fields:
+        mg.add_field(f)
+    mg.process()
+    mg.show()
+
+def cmd_graph(args):
+    '''graph command'''
+    usage = "usage: graph <FIELD...>"
+    if len(args) < 1:
+        print(usage)
+        return
+    if args[0][0] == ':':
+        i = int(args[0][1:])
+        g = mestate.graphs[i]
+        expression = g.expression
+        args = expression.split()
+        mestate.console.write("Added graph: %s\n" % g.name)
+        if g.description:
+            mestate.console.write("%s\n" % g.description, fg='blue')
+    mestate.console.write("Expression: %s\n" % ' '.join(args))
+    child = multiprocessing.Process(target=graph_process_vispy, args=[args])
+    child.start()
+
+def map_process(args):
+    '''process for a graph'''
+    from mavflightview import mavflightview_mav, mavflightview_options
+    mestate.mlog.reduce_by_flightmodes(mestate.flightmode_selections)
+    
+    options = mavflightview_options()
+    options.condition = mestate.settings.condition
+    if len(args) > 0:
+        options.types = ','.join(args)
+    mavflightview_mav(mestate.mlog, options)
+
+def cmd_map(args):
+    '''map command'''
+    child = multiprocessing.Process(target=map_process, args=[args])
+    child.start()
+
+def cmd_set(args):
+    '''control MAVExporer options'''
+    mestate.settings.command(args)
+
+def cmd_condition(args):
+    '''control MAVExporer conditions'''
+    if len(args) == 0:
+        print("condition is: %s" % mestate.settings.condition)
+        return
+    mestate.settings.condition = ' '.join(args)
+
+def cmd_reload(args):
+    '''reload graphs'''
+    load_graphs()
+    setup_menus()
+    mestate.console.write("Loaded %u graphs\n" % len(mestate.graphs))
+
+def cmd_param(args):
+    '''show parameters'''
+    if len(args) > 0:
+        wildcard = args[0]
+    else:
+        wildcard = '*'
+    k = sorted(mestate.mlog.params.keys())
+    for p in k:
+        if fnmatch.fnmatch(str(p).upper(), wildcard.upper()):
+            print("%-16.16s %f" % (str(p), mestate.mlog.params[p]))
+
+def process_stdin(line):
+    '''handle commands from user'''
+    if line is None:
+        sys.exit(0)
+
+    line = line.strip()
+    if not line:
+        return
+
+    args = line.split()
+    cmd = args[0]
+    if cmd == 'help':
+        k = command_map.keys()
+        k.sort()
+        for cmd in k:
+            (fn, help) = command_map[cmd]
+            print("%-15s : %s" % (cmd, help))
+        return
+    if cmd == 'exit':
+        mestate.exit = True
+        return
+
+    if not cmd in command_map:
+        print("Unknown command '%s'" % line)
+        return
+    (fn, help) = command_map[cmd]
+    try:
+        fn(args[1:])
+    except Exception as e:
+        print("ERROR in command %s: %s" % (args[1:], str(e)))
+
+def input_loop():
+    '''wait for user input'''
+    while mestate.exit != True:
+        try:
+            if mestate.exit != True:
+                line = raw_input(mestate.rl.prompt)
+        except EOFError:
+            mestate.exit = True
+            sys.exit(1)
+        mestate.input_queue.put(line)
+
+def main_loop():
+    '''main processing loop, display graphs and maps'''
+    while True:
+        if mestate is None or mestate.exit:
+            return
+        while not mestate.input_queue.empty():
+            line = mestate.input_queue.get()
+            cmds = line.split(';')
+            for c in cmds:
+                process_stdin(c)
+        time.sleep(0.1)
+
+command_map = {
+    'graph'      : (cmd_graph,     'display a graph'),
+    'set'        : (cmd_set,       'control settings'),
+    'reload'     : (cmd_reload,    'reload graphs'),
+    'condition'  : (cmd_condition, 'set graph conditions'),
+    'param'      : (cmd_param,     'show parameters'),
+    'map'        : (cmd_map,       'show map view'),
+    }
+
+mestate = MEState()
+mestate.rl = rline.rline("MAV> ", mestate)
+
+from argparse import ArgumentParser
+parser = ArgumentParser(description=__doc__)
+parser.add_argument("files", metavar="<FILE>", nargs="+")
+args = parser.parse_args()
+
+if len(args.files) == 0:
+    print("Usage: MAVExplorer FILE")
+    sys.exit(1)
+
+
+def progress_bar(pct):
+    if pct % 2 == 0:
+        mestate.console.write('#')
+
+mestate.console.write("Loading %s...\n" % args.files[0])
+t0 = time.time()
+mlog = mavutil.mavlink_connection(args.files[0], notimestamps=False,
+                                  zero_time_base=False)
+mestate.mlog = mavmemlog.mavmemlog(mlog, progress_bar)
+mestate.status.msgs = mlog.messages
+
+t1 = time.time()
+mestate.console.write("\ndone (%u messages in %.1fs)\n" % (mestate.mlog._count, t1-t0))
+  
+count_msg_types()
+
+load_graphs()
+setup_menus()
+
+# run main loop as a thread
+mestate.thread = threading.Thread(target=main_loop, name='main_loop')
+mestate.thread.daemon = True
+mestate.thread.start()
+
+# input loop
+while True:
+    try:
+        try:
+            line = raw_input(mestate.rl.prompt)
+        except EOFError:
+            mestate.exit = True
+            break
+        mestate.input_queue.put(line)
+    except KeyboardInterrupt:
+        mestate.exit = True
+        break
