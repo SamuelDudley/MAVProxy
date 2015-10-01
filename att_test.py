d4dc8de7026c8ad242eec37abc6665ad37f38839
SamuelDudley <dudley.samuel@gmail.com>
2015-10-01 13:02:28 +0930

    atts working for numpy arrays

---
+++ att_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+from math import *
+from numpy import *
+
+one = np.array([1,2,3])
+two = np.array([4,5,6])
+
+print sqrt(one**2+two**2)
+
+#ATT.DesRoll {'RAD': <pymavlink.DFReader.DFMessage object at 0x7f2d1e07a5d0>,
